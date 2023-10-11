bcb91a18f174edde5b2961fde1431880d6dc06a9
MihaelaBoian <pinzari_m@mail.ru>
2023-10-11 09:11:29 +0300

    n

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee ani=new Employee();
+        ani.setAge(17);
+        System.out.println(ani.getAge());
+    }
+}
